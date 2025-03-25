ce6f5b35b5a4038d8e48876017d974c4d3938ed0
Tim Kolber <tim.kolber@stud.uni-heidelberg.de>
2025-03-25 13:55:37 +0100

    Add data loading

---
+++ data.py
@@ -0,0 +1,44 @@
+from datasets import DatasetDict, load_dataset
+
+
+def load_data(dataset_identifier: str):
+    if dataset_identifier == "cais/mmlu":
+        dataset = load_dataset(dataset_identifier, "all", split="test")
+        dataset = dataset.rename_column("choices", "answers")
+        dataset = dataset.rename_column("answer", "correct_answer")
+    elif dataset_identifier == "tau/commonsense_qa":
+        dataset = load_dataset(dataset_identifier, split="test")
+        dataset = dataset.rename_column("choices", "answers")
+        dataset = dataset.rename_column("answerKey", "correct_answer")
+    elif dataset_identifier == "openlifescienceai/medmcqa":
+        dataset = load_dataset(dataset_identifier, split="test")
+        dataset = dataset.map(
+            lambda example: {
+                **example,
+                "answers": [
+                    example["opa"],
+                    example["opb"],
+                    example["opc"],
+                    example["opd"],
+                ],
+            }
+        )
+        dataset = dataset.map(
+            lambda example: {
+                **example,
+                "correct_answer": example["answers"][example["cop"]],
+            }
+        )
+    return dataset
+
+
+if __name__ == "__main__":
+    dataset_identifiers = [
+        "tau/commonsense_qa",
+        "cais/mmlu",
+        "openlifescienceai/medmcqa",
+    ]
+    for dataset_identifier in dataset_identifiers:
+        dataset = load_data(dataset_identifier)
+        print(len(dataset))
+        print(dataset[0])
--- ts_guessing.py
+++ ts_guessing.py
@@ -1,19 +1,22 @@
 import random
 
+from data import load_data
+
+
 def mask_wrong_answer(dataset):
     masked_data = []
-    
+
     for item in dataset:
         question = item["question"]
         answers = item["answers"]
         a_corr = item["correct_answer"]
-        
+
         wrong_answers = [a for a in answers if a != a_corr]
-        
-        #Check for sufficient amount of answers (ignore Yes/No and True/False prompts)
+
+        # Check for sufficient amount of answers (ignore Yes/No and True/False prompts)
         if len(wrong_answers) < 3:
             continue
-    
+
         a_masked = random.choice(wrong_answers)
 
         masked_options = []
@@ -24,19 +27,23 @@ def mask_wrong_answer(dataset):
                 masked_options.append("[MASK]")
             else:
                 masked_options.append(answer)
-        
+
         template = f"{question} {a_corr} {masked_options[0]} {masked_options[1]} {masked_options[2]}"
-        
-        masked_data.append({
-            "question": question,
-            "correct_answer": a_corr,
-            "masked_answer": a_masked,
-            "masked_template": template,
-        })
-    
+
+        masked_data.append(
+            {
+                "question": question,
+                "correct_answer": a_corr,
+                "masked_answer": a_masked,
+                "masked_template": template,
+            }
+        )
+
     return masked_data
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    dataset = load_data("tau/commonsense_qa")
     masked_output = mask_wrong_answer(dataset)
     for item in masked_output:
         print(item)
