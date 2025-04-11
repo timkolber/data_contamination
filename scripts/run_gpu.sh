181c72ff223bab737afc778721f52f8435f5b5ce
Tim Kolber <tim.kolber@stud.uni-heidelberg.de>
2025-04-11 09:10:54 +0200

    Add multi-masking results

--- scripts/run_gpu.sh
+++ scripts/run_gpu.sh
@@ -2,16 +2,18 @@
 #SBATCH --job-name=data_contamination_job
 #SBATCH --output=outputs/slurm/data_contamination_job-%j.out
 #SBATCH --error=outputs/slurm/data_contamination_job-%j.err
-#SBATCH --partition=gpu_4
-#SBATCH --time=1:00:00
+#SBATCH --partition=gpu_h100
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
 
-for ds in "ibragim-bad/arc_challenge" "ibragim-bad/arc_easy" "cais/mmlu"
+for ds in "ibragim-bad/arc_challenge" "ibragim-bad/arc_easy" "allenai/openbookqa"
 
 do
     for model in "meta-llama/Meta-Llama-3-8B-Instruct" "mistralai/Mistral-7B-Instruct-v0.3" "microsoft/phi-4"
     do
-        srun python main.py --model $model \
-        --dataset $ds
+        srun python main.py \
+        --model $model \
+        --dataset $ds \
+        --mask_amount 2
     done
 done
